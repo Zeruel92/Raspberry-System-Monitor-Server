a01f261cd8c187a5f7b0d9979f6e935ce5625874
Gabriele Tramonte <gabry.t92@gmail.com>
2019-03-09 11:24:21 +0100

    First Commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:raspberry_system_monitor_server/raspberry_system_monitor_server.dart';
+
+Future main() async {
+  final app = Application<RaspberrySystemMonitorServerChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 8888;
+
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ lib/raspberry_system_monitor_server.dart
@@ -0,0 +1,11 @@
+/// raspberry_system_monitor_server
+///
+/// A Aqueduct web server.
+library raspberry_system_monitor_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
