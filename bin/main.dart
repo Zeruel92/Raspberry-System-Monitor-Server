71ed189dc19f7375fe50896a4826c5429c739490
Gabriele Tramonte <gabry.t92@gmail.com>
2019-03-09 19:12:00 +0100

    Aggiunto broadcast message upd

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,23 @@
 import 'package:raspberry_system_monitor_server/raspberry_system_monitor_server.dart';
+import 'dart:convert';
 
 Future main() async {
   final app = Application<RaspberrySystemMonitorServerChannel>()
-      ..options.configurationFilePath = "config.yaml"
-      ..options.port = 8888;
+    ..options.configurationFilePath = "config.yaml"
+    ..options.port = 8888;
 
   final count = Platform.numberOfProcessors ~/ 2;
   await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  await RawDatagramSocket.bind(InternetAddress.anyIPv4, 8889)
+      .then((RawDatagramSocket udpSocket) {
+    udpSocket.broadcastEnabled = true;
+    final List<int> data = utf8.encode('rpi_broadcast_message');
+    Timer.periodic(Duration(seconds: 10), (_) {
+      udpSocket.send(data, InternetAddress('192.168.1.255'), 8889);
+      //print('Broadcasted');
+    });
+  });
 
   print("Application started on port: ${app.options.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
