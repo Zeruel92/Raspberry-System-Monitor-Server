f1a06d65f9fcaf4bc66dc73acc0e3e43372494b5
Gabriele Tramonte <gabry.t92@gmail.com>
2019-03-14 18:06:27 +0100

    uptime

--- lib/channel.dart
+++ lib/channel.dart
@@ -16,8 +16,27 @@ class RaspberrySystemMonitorServerChannel extends ApplicationChannel {
 
   Response _uptime(Request req) {
     ProcessResult result;
-    result = Process.runSync('bash', ['-c', 'uptime'],
+    result = Process.runSync(
+        'bash', ['-c', "uptime | tr \',\' \' \' | cut -d\" \" -f5"],
         includeParentEnvironment: true, runInShell: true);
-    return Response.ok(result.stdout);
+    final String uptime = result.stdout.toString();
+    result = Process.runSync('bash', ['-c', 'cut -d \' \' -f1 /proc/loadavg'],
+        includeParentEnvironment: true, runInShell: true);
+    final double loadAvg = double.parse(result.stdout.toString());
+    result = Process.runSync('bash', ['-c', 'cut -d \' \' -f2 /proc/loadavg'],
+        includeParentEnvironment: true, runInShell: true);
+    final double loadAvg5 = double.parse(result.stdout.toString());
+    result = Process.runSync('bash', ['-c', 'cut -d \' \' -f3 /proc/loadavg'],
+        includeParentEnvironment: true, runInShell: true);
+    final double loadAvg15 = double.parse(result.stdout.toString());
+    final Map<String, dynamic> headers = {};
+    headers["content-type"] = "application/json";
+    final Map<String, dynamic> body = {};
+    body['loadAvg'] = loadAvg;
+    body['time'] = uptime;
+    body['loadAvg5'] = loadAvg5;
+    body['loadAvg15'] = loadAvg15;
+    print(body);
+    return Response.ok(body, headers: headers);
   }
 }
