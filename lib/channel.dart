d2e4e2009d9aa8c3e7b863a65483966eaf329ac5
Gabriele Tramonte <gabry.t92@gmail.com>
2019-04-10 15:25:56 +0200

    todo on raspberry

--- lib/channel.dart
+++ lib/channel.dart
@@ -21,6 +21,7 @@ class RaspberrySystemMonitorServerChannel extends ApplicationChannel {
   }
 
   Response _uptime(Request req) {
+    //TODO verificare funzionamento su raspberry (su ubuntu manca vcgencmd )
     ProcessResult result;
     result = Process.runSync('bash', ['-c', 'uptime'],
         includeParentEnvironment: true, runInShell: true);
@@ -40,9 +41,14 @@ class RaspberrySystemMonitorServerChannel extends ApplicationChannel {
     result = Process.runSync(
         'bash', ['-c', '/opt/vc/bin/vcgencmd measure_temp'],
         includeParentEnvironment: true, runInShell: true);
+    print(result.stdout.toString());
+    print(result.stdout
+        .toString()
+        .substring(result.stdout.toString().indexOf('=') + 1));
     final double temp = double.parse(result.stdout
         .toString()
         .substring(result.stdout.toString().indexOf('=') + 1));
+
     final Map<String, dynamic> headers = {};
     headers["content-type"] = "application/json";
     final Map<String, dynamic> body = {};
